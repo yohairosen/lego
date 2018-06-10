075f05b090b815715bbeda180dfdc8d812a17d9a
Yohai Rosen <yohai.roz@gmail.com>
2018-06-10 14:11:44 +0300

    added MySQL driver tests

--- test/driver.js
+++ test/driver.js
@@ -1,75 +1,80 @@
 import Lego from '../src';
 import assert from 'assert';
-import { setPoolIdleTimeout } from '../src/driver/postgres';
-import { createDriver, getSingleton } from '../src/driver';
+import {setPoolIdleTimeout} from '../src/driver/postgres';
+import {createDriver, getSingleton} from '../src/driver';
+import url from 'url';
+
+const getProtocolData =
+    name => ({
+        mysql: {
+            NO_SUCH_FIELD_ERR: 'ER_BAD_FIELD_ERROR',
+            SELECT_WHERE_SQL: 'SELECT 1 FROM DUAL WHERE 1 <> 1',
+        }
+    }[name] || {
+        NO_SUCH_FIELD_ERR: '42703',
+        SELECT_WHERE_SQL: 'SELECT 1 WHERE 1 <> 1',
+    });
 
 describe('driver', function () {
-	it('exec and resolves', function () {
-		const lego = Lego.sql `SELECT 1 as count`;
-		return lego
-			.then(function (rows) {
-				assert.equal(rows.length, 1);
-				assert.equal(rows[0].count, '1');
-			});
-	});
+    const PROTOCOL = url.parse(process.env.DATABASE_URL).protocol.slice(0, -1);
+    const {NO_SUCH_FIELD_ERR, SELECT_WHERE_SQL} = getProtocolData(PROTOCOL);
+    it('exec and resolves', function () {
+        const lego = Lego.sql `SELECT 1 as count`;
+        return lego
+            .then(function (rows) {
+                assert.equal(rows.length, 1);
+                assert.equal(rows[0].count, '1');
+            });
+    });
 
-	it('exec and rejects', function () {
-		const lego = Lego.sql `SELECT fail`;
-		return lego
-			.then(assert.fail)
-			.catch(function (error) {
-				assert.equal(error.message, 'column "fail" does not exist');
-			});
-	});
+    it('exec and rejects', function () {
+        const lego = Lego.sql`SELECT fail`;
+        return lego
+            .then(assert.fail)
+            .catch(function (error) {
+                assert.equal(error.code, NO_SUCH_FIELD_ERR);
+            });
+    });
 
-	it('resolves first result', function () {
-		const lego = Lego.sql `SELECT 1 as count`;
-		return lego.first()
-			.then(function (row) {
-				assert.equal(row.length, undefined);
-				assert.equal(row.count, '1');
-			});
-	});
+    it('resolves first result', function () {
+        const lego = Lego.sql `SELECT 1 as count`;
+        return lego.first()
+            .then(function (row) {
+                assert.equal(row.length, undefined);
+                assert.equal(row.count, '1');
+            });
+    });
 
-	it('resolves null result with first on 0 rows', function () {
-		const lego = Lego.sql `SELECT 1 WHERE 1 <> 1`;
-		return lego.first()
-			.then(function (row) {
-				assert.equal(row, null);
-			});
-	});
+    it('resolves null result with first on 0 rows', function () {
+        return Lego.sql`${Lego.raw(SELECT_WHERE_SQL)}`.first()
+            .then(function (row) {
+                assert.equal(row, null);
+            });
+    });
 
-	it('is then-able', function () {
-		return Lego.sql `SELECT 1 as count`
-			.then(function (rows) {
-				assert.equal(rows.length, 1);
-				assert.equal(rows[0].count, '1');
-			});
-	});
+    it('is then-able', function () {
+    	return Lego.sql `SELECT 1 as count`
+    		.then(function (rows) {
+    			assert.equal(rows.length, 1);
+    			assert.equal(rows[0].count, '1');
+    		});
+    });
 
-	it('can set pool idle timeout', function () {
-		assert.doesNotThrow(() => {
-			setPoolIdleTimeout(500);
-		});
-	});
+    it('can set pool idle timeout', function () {
+    	assert.doesNotThrow(() => {
+    		setPoolIdleTimeout(500);
+    	});
+    });
 
-	it('undefined database url', function () {
-		assert.throws(function () {
-			createDriver(null);
-		}, function (error) {
-			return error.message === 'No DATABASE_URL provided.';
-		});
-	});
+    it('undefined database url', function () {
+    	assert.throws(function () {
+    		createDriver(null);
+    	}, function (error) {
+    		return error.message === 'No DATABASE_URL provided.';
+    	});
+    });
 
-	it('unsupported driver', function () {
-		assert.throws(function () {
-			createDriver('mysql://localhost');
-		}, function (error) {
-			return error.message === 'Unsupported driver \'mysql:\' in DATABASE_URL.';
-		});
-	});
-
-	it('get singleton', function () {
-		assert.strictEqual(getSingleton(), getSingleton());
-	});
+    it('get singleton', function () {
+    	assert.strictEqual(getSingleton(), getSingleton());
+    });
 });
--- test/transaction.js
+++ test/transaction.js
@@ -1,94 +1,119 @@
 import Lego from '../src';
 import assert from 'assert';
+import url from 'url';
+
+const getProtocolData =
+    name => ({
+        mysql: {
+            CREATE_TABLE_SQL: 'CREATE TABLE tests (text VARCHAR(100), value INTEGER, UNIQUE KEY(text))',
+            INSERT_ROW_SQL: `INSERT INTO tests (text, value) VALUES ('Martijn', 123)`,
+            DUP_ENTRY_ERR: 'ER_DUP_ENTRY'
+        }
+    }[name] || {
+        CREATE_TABLE_SQL: 'CREATE TABLE tests (text TEXT UNIQUE, value INTEGER)',
+        INSERT_ROW_SQL: `INSERT INTO tests (text, value) VALUES ('Martijn', 123) RETURNING *`,
+        DUP_ENTRY_ERR: '23505'
+    });
+
 
 describe('transaction', function () {
-	beforeEach(function () {
-		return Lego.sql `CREATE TABLE tests (text TEXT UNIQUE, value INTEGER)`;
-	});
+    const PROTOCOL = url.parse(process.env.DATABASE_URL).protocol.slice(0, -1);
 
-	afterEach(function () {
-		return Lego.sql `DROP TABLE IF EXISTS tests`;
-	});
+    const {CREATE_TABLE_SQL, INSERT_ROW_SQL, DUP_ENTRY_ERR} = getProtocolData(PROTOCOL);
 
-	it('can commit', function () {
-		return Lego.transaction(function (transaction) {
-				return transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 123) RETURNING *`
-					.then(function (tests) {
-						assert.equal(tests.length, 1);
-						assert.equal(tests[0].text, 'Martijn');
-						assert.equal(tests[0].value, 123);
-					});
-			})
-			.then(function () {
-				return Lego.sql `SELECT * FROM tests`;
-			})
-			.then(function (tests) {
-				assert.equal(tests.length, 1);
-				assert.equal(tests[0].text, 'Martijn');
-				assert.equal(tests[0].value, 123);
-			});
-	});
+    const returning = fn => {
+        return PROTOCOL === 'mysql' ? fn : res => res;
+    };
 
-	it('can rollback', function () {
-		return Lego.transaction(function (transaction) {
-				return transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 123) RETURNING *`
-					.then(function (tests) {
-						assert.equal(tests.length, 1);
-						assert.equal(tests[0].text, 'Martijn');
-						assert.equal(tests[0].value, 123);
+    beforeEach(function () {
+        return Lego.sql `${Lego.raw(CREATE_TABLE_SQL)}`;
+    });
 
-						return transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 123) RETURNING *`;
-					});
-			})
-			.then(assert.fail)
-			.catch(function (error) {
-				assert.equal(error.code, '23505');
+    afterEach(function () {
+        return Lego.sql `DROP TABLE IF EXISTS tests`;
+    });
 
-				return Lego.sql `SELECT * FROM tests`
-					.then(function (tests) {
-						assert.equal(tests.length, 0);
-					});
-			});
-	});
+    it('can commit', function () {
+        return Lego.transaction(function (transaction) {
+            return transaction.sql `${Lego.raw(INSERT_ROW_SQL)}`
+                .then(returning(() => transaction.sql `SELECT * FROM tests`))
+                .then(function (tests) {
+                    assert.equal(tests.length, 1);
+                    assert.equal(tests[0].text, 'Martijn');
+                    assert.equal(tests[0].value, 123);
+                });
+        })
+            .then(function () {
+                return Lego.sql `SELECT * FROM tests`;
+            })
+            .then(function (tests) {
+                assert.equal(tests.length, 1);
+                assert.equal(tests[0].text, 'Martijn');
+                assert.equal(tests[0].value, 123);
+            });
+    });
 
-	it('should throw error in empty transaction', function () {
-		return Lego.transaction(function () {
-				// We return nothing so this fails.
-			})
-			.then(assert.fail)
-			.catch(function (error) {
-				assert.equal(error.message, '0 queries were found in Lego#transaction\'s callback.');
-			});
-	});
+    it('can rollback', function () {
+        return Lego.transaction(function (transaction) {
+            return transaction.sql `${Lego.raw(INSERT_ROW_SQL)}`
+                .then(returning(() => transaction.sql `SELECT * FROM tests`))
+                .then(function (tests) {
+                    assert.equal(tests.length, 1);
+                    assert.equal(tests[0].text, 'Martijn');
+                    assert.equal(tests[0].value, 123);
 
-	it('can execute multiple statements', function () {
-		return Lego
-			.transaction((transaction) => {
-				transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 1)`;
-				transaction.sql `UPDATE tests SET value = 2 WHERE value = 1`;
-				transaction.sql `UPDATE tests SET value = 3 WHERE value = 2`;
-			})
-			.then(() => {
-				return Lego.sql `SELECT * FROM tests`.first();
-			})
-			.then((test) => {
-				assert.equal(test.value, 3);
-				assert.equal(test.text, 'Martijn');
-			});
-	});
+                    return transaction.sql `${Lego.raw(INSERT_ROW_SQL)}`;
+                });
+        })
+            .then(assert.fail)
+            .catch(function (error) {
+                assert.equal(error.code, DUP_ENTRY_ERR);
 
-	it('should execute in series with return value', function () {
-		return Lego
-			.transaction((transaction) => {
-				transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 1)`;
-				return transaction.sql `UPDATE tests SET value = 2 WHERE value = 1`;
-			})
-			.then(() => {
-				return Lego.sql `SELECT * FROM tests`.first();
-			})
-			.then((test) => {
-				assert.equal(test.value, 2);
-				assert.equal(test.text, 'Martijn');
-			});
-	});
+                return Lego.sql `SELECT * FROM tests`
+                    .then(function (tests) {
+                        assert.equal(tests.length, 0);
+                    });
+            });
+    });
+
+    it('should throw error in empty transaction', function () {
+        return Lego.transaction(function () {
+            // We return nothing so this fails.
+        })
+            .then(assert.fail)
+            .catch(function (error) {
+                assert.equal(error.message, '0 queries were found in Lego#transaction\'s callback.');
+            });
+    });
+
+    it('can execute multiple statements', function () {
+        return Lego
+            .transaction((transaction) => {
+                transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 1)`;
+                transaction.sql `UPDATE tests SET value = 2 WHERE value = 1`;
+                transaction.sql `UPDATE tests SET value = 3 WHERE value = 2`;
+            })
+            .then(() => {
+                return Lego.sql `SELECT * FROM tests`.first();
+            })
+            .then((test) => {
+                assert.equal(test.value, 3);
+                assert.equal(test.text, 'Martijn');
+            });
+    });
+
+    it('should execute in series with return value', function () {
+        return Lego
+            .transaction((transaction) => {
+                transaction.sql `INSERT INTO tests (text, value) VALUES ('Martijn', 1)`;
+                return transaction.sql `UPDATE tests SET value = 2 WHERE value = 1`;
+            })
+            .then(() => {
+                return Lego.sql `SELECT * FROM tests`.first();
+            })
+            .then((test) => {
+                assert.equal(test.value, 2);
+                assert.equal(test.text, 'Martijn');
+            });
+    });
 });
